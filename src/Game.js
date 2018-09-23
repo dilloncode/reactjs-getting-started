fc3225827745ae3d897b7b2b92b91a71fa46de48
Dillon <mdillon@deloitte.com>
2018-09-23 11:22:40 -0400

    add state

--- src/Game.js
+++ src/Game.js
@@ -3,22 +3,24 @@ import React, { Component } from 'react';
 import { Stars, Button, Answer, Numbers } from './components'
 
 class Game extends Component {
-    state = {  }
-    render() { 
-        return ( 
+    state = {
+        selectedNumbers: []
+    };
+    render() {
+        return (
             <div className="container">
                 <h3>Play Nine</h3>
                 <hr />
                 <div className="row">
                     <Stars />
                     <Button />
-                    <Answer />
+                    <Answer selectedNumbers={this.state.selectedNumbers} />
                 </div>
                 <br />
-                <Numbers />
+                <Numbers selectedNumbers={this.state.selectedNumbers} />
             </div>
-         );
+        );
     }
 }
- 
+
 export default Game;
--- src/components.js
+++ src/components.js
@@ -3,44 +3,50 @@ import range from 'lodash/range';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Stars = (props) => {
-    const numberOfStars = 1 + Math.floor(Math.random() * 9);
+  const numberOfStars = 1 + Math.floor(Math.random() * 9);
 
-    return (
-        <div className="col-5">
-            {range(numberOfStars).map(i =>
-                <FontAwesomeIcon icon="star" key={i} />
-            )}
-        </div>
-    );
+  return (
+    <div className="col-5">
+      {range(numberOfStars).map(i =>
+        <FontAwesomeIcon icon="star" key={i} />
+      )}
+    </div>
+  );
 }
 
 export const Button = (props) => {
-    return (
-        <div className="col-2">
-            <button>=</button>
-        </div>
-    );
+  return (
+    <div className="col-2">
+      <button>=</button>
+    </div>
+  );
 }
 
 export const Answer = (props) => {
-    return (
-        <div className="col-5">
-            <span>5</span>
-            <span>6</span>
-        </div>
-    );
+  return (
+    <div className="col-5">
+      {props.selectedNumbers.map((number, i) =>
+        <span key={i}>{number}</span>
+      )}
+    </div>
+  );
 }
 
 export const Numbers = (props) => {
-    return (
-        <div className="card text-center">
-            <div>
-                {Numbers.list.map((number, i) =>
-                    <span key={i}>{number}</span>
-                )}
-            </div>
-        </div>
-    );
+  const numberClassName = (number) => {
+    if (props.selectedNumbers.includes(number)) {
+      return 'selected';
+    }
+  }
+  return (
+    <div className="card text-center">
+      <div>
+        {Numbers.list.map((number, i) =>
+          <span key={i} className={numberClassName(number)}>{number}</span>
+        )}
+      </div>
+    </div>
+  );
 }
 
 Numbers.list = range(1, 10);
