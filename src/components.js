5e2192df7fcb4132bddde32d7420452ce0fa3ee5
Dillon <mdillon@deloitte.com>
2018-09-23 11:01:57 -0400

    added lodash range

--- src/components.js
+++ src/components.js
@@ -1,18 +1,15 @@
 import React from 'react';
+import range from 'lodash/range';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Stars = (props) => {
-    const numberOfStars = 1 + Math.floor(Math.random()*9);
-
-    let stars = [];
-
-    for(let i=0; i<numberOfStars; i++) {
-        stars.push(<FontAwesomeIcon icon="star" />)
-    }
+    const numberOfStars = 1 + Math.floor(Math.random() * 9);
 
     return (
         <div className="col-5">
-            {stars}
+            {range(numberOfStars).map(i =>
+                <FontAwesomeIcon icon="star" key={i} />
+            )}
         </div>
     );
 }
@@ -28,19 +25,22 @@ export const Button = (props) => {
 export const Answer = (props) => {
     return (
         <div className="col-5">
-            ...
+            <span>5</span>
+            <span>6</span>
         </div>
     );
 }
 
 export const Numbers = (props) => {
-    return ( 
+    return (
         <div className="card text-center">
             <div>
-                <span>1</span>
-                <span className="selected">2</span>
-                <span className="used">3</span>
+                {Numbers.list.map((number, i) =>
+                    <span key={i}>{number}</span>
+                )}
             </div>
         </div>
-     );
+    );
 }
+
+Numbers.list = range(1, 10);
