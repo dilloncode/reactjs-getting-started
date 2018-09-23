9498f38c23eb7b74249399e8a71bcce916f43bce
Dillon <mdillon@deloitte.com>
2018-09-23 09:51:17 -0400

    basic game components

---
+++ src/Game.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h3>Play Nine</h3>
+            </div>
+         );
+    }
+}
+ 
+export default Game;
---
+++ src/components.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Stars = (props) => {
+    return (
+        <div>
+            ...
+        </div>
+    );
+}
+
+export const Button = (props) => {
+    return (
+        <div>
+            ...
+        </div>
+    );
+}
+
+export const Answer = (props) => {
+    return (
+        <div>
+            ...
+        </div>
+    );
+}
