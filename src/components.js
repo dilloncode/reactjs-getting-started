5cd62fff48972cc7ed367800174648b0fe7b35f7
Dillon <mdillon@deloitte.com>
2018-09-23 10:50:48 -0400

    add random stars

--- src/components.js
+++ src/components.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Stars = (props) => {
+    const numberOfStars = 1 + Math.floor(Math.random()*9);
+
+    let stars = [];
+
+    for(let i=0; i<numberOfStars; i++) {
+        stars.push(<FontAwesomeIcon icon="star" />)
+    }
+
     return (
         <div className="col-5">
-            <FontAwesomeIcon icon="star" />
-            <FontAwesomeIcon icon="star" />
-            <FontAwesomeIcon icon="star" />
-            <FontAwesomeIcon icon="star" />
+            {stars}
         </div>
     );
 }
